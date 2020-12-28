6175b20a717d004192e6f7282a0b24d92b3ebfcf
Oleksandr Tereshchenko <toporok@gmail.com>
2020-12-28 22:14:48 +0200

    does es indexing work?

--- functions/common/thoughts.py
+++ functions/common/thoughts.py
@@ -1,4 +1,7 @@
 import logging
+from pprint import pformat
+
+from functions.common.elasticsearch import create_es_client, THOUGHTS_ES_IDX
 
 logger = logging.getLogger(__name__)
 
@@ -10,7 +13,22 @@ def construct_thought_id(msg_id, chat_id, bot_id):
 
 
 def index_thought(text, msg_id, chat_id, bot_id):
-    pass
+    es = create_es_client()
+    # TODO oleksandr: create index if doesn't exist ?
+
+    thought_id = construct_thought_id(msg_id=msg_id, chat_id=chat_id, bot_id=bot_id)
+    response = es.index(
+        index=THOUGHTS_ES_IDX,
+        id=thought_id,
+        body={
+            'answer_thought_id': thought_id,
+            'answer': text,
+        }
+    )
+
+    if logger.isEnabledFor(logging.INFO):
+        logger.info('INDEX THOUGHT ES RESPONSE:\n%s', pformat(response))
+    return response
 
 
 def answer_thought(text):
