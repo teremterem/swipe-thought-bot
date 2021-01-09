4079c7de9b78327fa01aa3d7765b815f4d37291e
Oleksandr Tereshchenko <toporok@gmail.com>
2021-01-09 03:39:06 +0200

    multiple environments in sls

--- helper_tools/helper_utils.py
+++ helper_tools/helper_utils.py
@@ -9,6 +9,8 @@ def set_env_vars(project_dir='../', backend_stage='oleksandr', local_overlay=Fal
     if local_overlay:
         set_env_vars_from_yml(f"{project_dir}serverless.env-local.yml")
 
+    os.environ.setdefault('STAGE', backend_stage)
+    os.environ.setdefault('REGION', 'us-east-1')
     os.environ.setdefault('ES_REGION', os.environ['REGION'])
     os.environ.setdefault('SWIPER_CHAT_DATA_DDB_TABLE_NAME', f"SwiperChatData-stb-{os.environ['STAGE']}")
     os.environ.setdefault('MAIN_S3_BUCKET_NAME', f"stb-{os.environ['STAGE']}")
