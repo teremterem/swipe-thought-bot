c57ec7517763cb31e94bb0e4b3226be01a8803a5
Oleksandr Tereshchenko <toporok@gmail.com>
2021-01-12 02:15:05 +0200

    in progress: swiper two - send message to someone

--- functions/swiper_experiments/swiper_two/swiper_two.py
+++ functions/swiper_experiments/swiper_two/swiper_two.py
@@ -5,11 +5,10 @@ from pprint import pformat
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
-from functions.common.constants import ConvState, DataKey, EsKey, AnswererMode
+from functions.common.constants import ConvState, DataKey
 from functions.common.s3 import main_bucket
 from functions.common.swiper_telegram import BaseSwiperConversation, StateAwareHandlers, BaseSwiperPresentation
 from functions.swiper_experiments.swiper_one.swiper_one import send_internals, is_bot_silent
-from functions.swiper_experiments.swiper_one.thoughts import construct_thought_id, ThoughtContext
 
 logger = logging.getLogger(__name__)
 
@@ -54,83 +53,31 @@ class CommonStateHandlers(StateAwareHandlers):
             self.swiper_presentation.say_hello(update, context, self.conv_state)
 
     def user_thought(self, update, context):
-        bot_id = context.bot.id
-        chat_id = update.effective_chat.id
         user_msg_id = update.effective_message.message_id
 
         text = update.effective_message.text
-        thought_id = construct_thought_id(msg_id=user_msg_id, chat_id=chat_id, bot_id=bot_id)
 
-        thought_ctx = ThoughtContext(context)
+        if not is_bot_silent(context):  # TODO oleksandr: rename to is_swiper_authorized
+            # TODO oleksandr: indexing goes inside this if statement
 
-        answerer = self.swiper_conversation.get_answerer()
-        answerer.index_thought(answer_text=text, answer_thought_id=thought_id, thought_ctx=thought_ctx)
-
-        who_replied = ConvState.USER_REPLIED
-        thought_ctx.append_thought(
-            text=text,
-            thought_id=thought_id,
-            msg_id=user_msg_id,
-            conv_state=who_replied,
-        )
-
-        if is_bot_silent(context):
-            answer_dict = None
-        else:
-            answerer_mode = context.chat_data.setdefault(DataKey.ANSWERER_MODE, AnswererMode.DEFAULT)
-            answer_dict = answerer.answer(thought_ctx, answerer_mode)
-
-        if answer_dict:
-            answer_text = answer_dict[EsKey.ANSWER]
-            answer_thought_id = answer_dict[EsKey.ANSWER_THOUGHT_ID]
-
-            bot_msg = self.swiper_presentation.answer_thought(update, context, answer_text)
-
-            who_replied = ConvState.BOT_REPLIED
-            thought_ctx.append_thought(
-                text=answer_text,
-                thought_id=answer_thought_id,
-                msg_id=bot_msg.message_id,
-                conv_state=who_replied,
-            )
+            bot_msg = self.swiper_presentation.answer_thought(update, context, text)
 
             context.chat_data[DataKey.LATEST_ANSWER_MSG_ID] = bot_msg.message_id
             context.chat_data[DataKey.LATEST_MSG_ID] = bot_msg.message_id
-        else:
-            context.chat_data[DataKey.LATEST_MSG_ID] = user_msg_id
+            return ConvState.BOT_REPLIED
 
-        thought_ctx.trim_context()
-
-        # # Move the following code to SwiperPresentation if you decide to uncomment it...
-        # if previous_latest_answer_msg_id:
-        #     try:
-        #         bot.edit_message_reply_markup(
-        #             message_id=latest_answer_msg_id,
-        #             chat_id=chat_id,
-        #             reply_markup=InlineKeyboardMarkup(inline_keyboard=[]),
-        #         )
-        #     except Exception:
-        #         logger.info('INLINE KEYBOARD DID NOT SEEM TO NEED REMOVAL', exc_info=True)
-
-        return who_replied
+        context.chat_data[DataKey.LATEST_MSG_ID] = user_msg_id
+        return ConvState.USER_REPLIED
 
     def like(self, update, context):
         self.swiper_presentation.like(update, context)
 
     def dislike(self, update, context):
         if update.effective_message.message_id == context.chat_data.get(DataKey.LATEST_MSG_ID):
-            thought_ctx = ThoughtContext(context)
-
-            thought_ctx.reject_latest_thought(validate_msg_id=update.effective_message.message_id)
-
-            previous_state = thought_ctx.get_latest_conv_state()
-            if not previous_state:
-                # empty previous_state is unlikely
-                previous_state = ConversationHandler.END  # I assume this would clear conversation state
 
             self.swiper_presentation.reject(update, context)
 
-            return previous_state
+            return ConvState.USER_REPLIED
         else:
             self.swiper_presentation.dislike(update, context)
 
