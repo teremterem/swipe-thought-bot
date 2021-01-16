0455a845d0cea2647d30997198058b68e6573046
Oleksandr Tereshchenko <toporok@gmail.com>
2021-01-16 12:41:51 +0200

    turn is_swiper_authorized into assert_swiper_authorized handler

--- functions/swiper_experiments/swiper_three/swiper_prototype.py
+++ functions/swiper_experiments/swiper_three/swiper_prototype.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, DispatcherHandlerStop, Filters, MessageHandler
 
 from functions.common.constants import DataKey
 from functions.common.swiper_telegram import BaseSwiperConversation
@@ -15,16 +15,17 @@ SWIPER3_CHAT_ID = os.environ['SWIPER3_CHAT_ID']
 
 class SwiperPrototype(BaseSwiperConversation):
     def configure_dispatcher(self, dispatcher):
+        dispatcher.add_handler(MessageHandler(Filters.all, self.assert_swiper_authorized), -1)
+
         dispatcher.add_handler(CommandHandler('start', self.start))
 
-    def is_swiper_authorized(self):
+    def assert_swiper_authorized(self, update, context):
         # single-threaded environment with non-async update processing
-        return bool(self.swiper_update.swiper_chat_data.get(DataKey.IS_SWIPER_AUTHORIZED))
+        if not self.swiper_update.swiper_chat_data.get(DataKey.IS_SWIPER_AUTHORIZED):
+            # https://github.com/python-telegram-bot/python-telegram-bot/issues/849#issuecomment-332682845
+            raise DispatcherHandlerStop()
 
     def start(self, update, context):
-        if not self.is_swiper_authorized():
-            return
-
         context.bot.send_message(
             chat_id=SWIPER1_CHAT_ID,
             text='Hello Swiper One',
