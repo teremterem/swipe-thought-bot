6422abcef1df76d882bc735c6c92e792c87b2ccb
Oleksandr Tereshchenko <toporok@gmail.com>
2021-02-28 15:08:03 +0200

    create topic and allogrooming table objects in dynamodb.py

--- functions/common/dynamodb.py
+++ functions/common/dynamodb.py
@@ -10,11 +10,15 @@ REGION = os.environ['REGION']
 
 SWIPER_CHAT_DATA_DDB_TABLE_NAME = os.environ['SWIPER_CHAT_DATA_DDB_TABLE_NAME']
 MESSAGE_TRANSMISSION_DDB_TABLE_NAME = os.environ['MESSAGE_TRANSMISSION_DDB_TABLE_NAME']
+TOPIC_DDB_TABLE_NAME = os.environ['TOPIC_DDB_TABLE_NAME']
+ALLOGROOMING_DDB_TABLE_NAME = os.environ['ALLOGROOMING_DDB_TABLE_NAME']
 
 dynamodb = boto3.resource('dynamodb', region_name=REGION)
 
 swiper_chat_data_table = dynamodb.Table(SWIPER_CHAT_DATA_DDB_TABLE_NAME)
 msg_transmission_table = dynamodb.Table(MESSAGE_TRANSMISSION_DDB_TABLE_NAME)
+topic_table = dynamodb.Table(MESSAGE_TRANSMISSION_DDB_TABLE_NAME)
+allogrooming_table = dynamodb.Table(ALLOGROOMING_DDB_TABLE_NAME)
 
 
 # TODO oleksandr: get rid of put_ddb_item and delete_ddb_item functions - I don't think you need them
