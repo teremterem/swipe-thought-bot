eac50ce3da5c853bd4dc2c45cb4dcb033e666e6b
Oleksandr Tereshchenko <toporok@gmail.com>
2021-03-10 21:56:49 +0200

    kind_of_random_username function

---
+++ functions/swiper_experiments/swiper_username.py
@@ -0,0 +1,12 @@
+import random
+
+
+def kind_of_random_username():
+    kind_of_random_set = [
+        'Komatsu-Chiyuki-5580',
+        'Otonari-Ichi-9984',
+        'Eto-Utzuki-3786',
+        'Haruyama-Torio-6952',
+        'Kashiwagi-Sosa-3923',
+    ]
+    return random.choice(kind_of_random_set)
