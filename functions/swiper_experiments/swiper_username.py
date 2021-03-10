917168f5e9c003a15945350d3008c1f1975b92f9
Oleksandr Tereshchenko <toporok@gmail.com>
2021-03-10 22:17:30 +0200

    capture telegram update with "username" entity to mimic this entity later

--- functions/swiper_experiments/swiper_username.py
+++ functions/swiper_experiments/swiper_username.py
@@ -1,3 +1,37 @@
+"""
+INFO:functions.common.utils:LAMBDA EVENT:
+{'body': {'message': {'caption': 'Идти уже домой или нет?\n\nEto-Utzuki-3786',
+                      'caption_entities': [{'length': 15,
+                                            'offset': 25,
+                                            'type': 'italic'}],
+                      'chat': {'first_name': 'teremterem',
+                               'id': 210723289,
+                               'type': 'private',
+                               'username': 'teremterem'},
+                      'date': 1615407165,
+                      'from': {'first_name': 'teremterem',
+                               'id': 210723289,
+                               'is_bot': False,
+                               'language_code': 'uk',
+                               'username': 'teremterem'},
+                      'message_id': 3099,
+                      'photo': [{'file_id': 'AgACAgIAAxkBAAIMG2BJKD3OT1I9...o3gE6Q6bQ2GGaJni4AAwEAAwIAA20AA9OPAwABHgQ',
+                                 'file_size': 6115,
+                                 'file_unique_id': 'AQADGGaJni4AA9OPAwAB',
+                                 'height': 320,
+                                 'width': 256},
+                                {'file_id': 'AgACAgIAAxkBAAIMG2BJKD3OT1I9...o3gE6Q6bQ2GGaJni4AAwEAAwIAA3gAA9SPAwABHgQ',
+                                 'file_size': 27340,
+                                 'file_unique_id': 'AQADGGaJni4AA9SPAwAB',
+                                 'height': 800,
+                                 'width': 640},
+                                {'file_id': 'AgACAgIAAxkBAAIMG2BJKD3OT1I9...o3gE6Q6bQ2GGaJni4AAwEAAwIAA3kAA9GPAwABHgQ',
+                                 'file_size': 41475,
+                                 'file_unique_id': 'AQADGGaJni4AA9GPAwAB',
+                                 'height': 1280,
+                                 'width': 1024}]},
+          'update_id': 284002334}}
+"""
 import random
 
 
